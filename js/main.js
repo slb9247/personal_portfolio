8eede9735cfacc33dcee26de133b362007af23f7
Samantha Bolotine <slb1992@yahoo.com>
2016-06-19 21:38:57 -0400

    first

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).on('ready', function() {
+
+  // $(window).on('resize', function (){
+  //   $('#splashTop').height($('#img-splashTop').height());
+  // });
+
+
+
+
+
+
+
+
+
+
+
+
+}); // ends doc.ready
