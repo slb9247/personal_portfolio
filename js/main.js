49a5eeaf3504a95877dcbb40633a40cca58640d9
Samantha Bolotine <slb1992@yahoo.com>
2016-06-23 16:40:33 -0400

    fade in animation

--- js/main.js
+++ js/main.js
@@ -1,13 +1,27 @@
 $(document).on('ready', function() {
 
+// specifies current page
+  $('nav a').on('click', function() {
+    $('nav a').removeClass('current');
+    $(this).addClass('current');
+  });
 
 
+
+// home page transition animations
   $('#link-about, #link-design, #link-dev').on('click', function() {
-    $('#splashTop').addClass('animate_elem animate_elem_slideup');
 
-    setTimeout(function() {
-      $('#splashTop').removeClass('animate_elem animate_elem_slideup');
-    }, 5000);
+      $('#splash').addClass('animate_elem_slideup');
+
+      setTimeout(function() {
+        $('#splash').removeClass('animate_elem_slideup');
+      }, 5000); // event listener?
+  });
+
+// design page fade in
+  $('#link-design').on('click', function() {
+
+    $('#page-design').fadeIn(5000);
   });
 
 
